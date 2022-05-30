23d6412ed03520034f42273b4b7a971b2a2a95ec
batsdk <shemilkaweesha@gmail.com>
2022-05-30 14:04:38 +0530

    Auth Controllers

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -51,7 +51,11 @@ const login = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 const logout = async (req, res) => {
-  res.send("logout works");
+  res.cookie("token", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now() + 2000),
+  });
+  res.end();
 };
 
 module.exports = { register, login, logout };
