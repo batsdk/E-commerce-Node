fd1e548f77515fd22698271d2fd1457b94204aef
batsdk <shemilkaweesha@gmail.com>
2022-06-10 23:22:28 +0530

    Order Model and Controllers

--- Controllers/orderController.js
+++ Controllers/orderController.js
@@ -1,4 +1,35 @@
+const Order = require("../Models/Order");
+const Product = require("../Models/Product");
+
+const User = require("../Models/User");
+const Errors = require("../errors");
+
+const { checkPermission } = require("../Utils");
+const { StatusCodes } = require("http-status-codes");
+
+//! Create Order
 const createOrder = async (req, res) => {
+  const { items: cartItems, tax, shippingFee } = req.body;
+
+  if (!cartItems || cartItems.length < 1) {
+    throw new Errors.BadRequestError("No cart items provided.");
+  }
+
+  if (!tax || !shippingFee) {
+    throw new Errors.BadRequestError("Please provide shipping fee and tax ");
+  }
+
+  let orderItems = [];
+  let subtotal = 0;
+
+  for (const item of cartItems) {
+    const dbProduct = await Product.findOne({ _id: item.product });
+    if (!dbProduct) {
+      throw new Errors.NotFoundError("There is no product");
+    }
+    const { name, price, image, _id } = dbProduct;
+  }
+
   res.send("Create");
 };
 const getAllOrders = async (req, res) => {
